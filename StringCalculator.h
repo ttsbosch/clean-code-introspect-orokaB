0abf30677c221e91d7be333fa069e81b9c71f716
orokaB <164506573+orokaB@users.noreply.github.com>
2024-06-27 15:39:05 +0530

    refactoring StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,18 @@
-#include <iostream>
-#include <string>
+// StringCalculator.h
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
 
-using namespace std;
-class StringCalculator{
-  public:
-  int add(std::string& numbers);
+#include <string>
+#include <vector>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    std::vector<int> parseNumbers(const std::string& numbers);
+    void checkForNegatives(const std::vector<int>& numbers);
+    int sumNumbers(const std::vector<int>& numbers);
 };
+
+#endif // STRINGCALCULATOR_H
