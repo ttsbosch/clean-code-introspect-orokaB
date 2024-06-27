7d2afa0cd4580d4b7661ebd488154eafbbfa3f0f
orokaB <164506573+orokaB@users.noreply.github.com>
2024-06-27 15:15:32 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,7 +1,16 @@
 #include "StringCalculator.h"
-int StringCalculator::add(string input){
-  if (input == "")
-  {
-    return 0;
-  }
+int StringCalculator::add(std::string& numbers){
+  if (numbers.empty()) {
+        return 0;
+    }
+
+    std::stringstream ss(numbers);
+    std::string item;
+    int sum = 0;
+
+    while (std::getline(ss, item, ',')) {
+        sum += std::stoi(item);
+    }
+
+    return sum;
 }
