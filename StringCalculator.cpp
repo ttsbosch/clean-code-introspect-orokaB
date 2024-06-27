e7333f6f05cd97fedf58bc8a9b8aff888c704d3f
orokaB <164506573+orokaB@users.noreply.github.com>
2024-06-27 15:31:16 +0530

    updated to handle negative numbers StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -9,10 +9,28 @@ int StringCalculator::add(std::string& numbers){
     std::stringstream ss(numbers);
     std::string item;
     int sum = 0;
-
+    std::vector<int> negatives;
+  
     while (std::getline(ss, item, ',')) {
-        sum += std::stoi(item);
+        int number = std::stoi(item);
+        if (number < 0) {
+            negatives.push_back(number);
+        } else {
+            sum += number;
+        }
     }
+
+    if (!negatives.empty()) {
+        std::string errorMessage = "negatives not allowed: ";
+        for (size_t i = 0; i < negatives.size(); ++i) {
+            errorMessage += std::to_string(negatives[i]);
+            if (i < negatives.size() - 1) {
+                errorMessage += ", ";
+            }
+        }
+        throw std::runtime_error(errorMessage);
+    }
+   
 
     return sum;
 }
